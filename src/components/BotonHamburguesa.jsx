801590a214ab6e596b1789faa6f7ff8bcba59442
CarlosFranco97 <127879068+CarlosFranco97@users.noreply.github.com>
2025-03-10 13:49:57 -0500

    Se mejora boton añadiendole a las lineas position absolute, en todas las pantallas

--- src/components/BotonHamburguesa.jsx
+++ src/components/BotonHamburguesa.jsx
@@ -1,18 +1,18 @@
 const BotonHamburguesa = ({ alternarMenu, menuAbierto }) => {
     return (
         <div
-            className={`flex flex-col justify-evenly items-center mr-4 w-15 h-15 xl:h-25 xl:w-30 cursor-pointer z-10`}
+            className={`relative flex flex-col justify-evenly items-center mr-4 w-15 h-12 cursor-pointer z-30 xl:h-22 xl:w-30`}
             onClick={alternarMenu}
             >
-            <span className={`w-[50px] lg:w-[60px] xl:w-[85px] h-[5px] lg:h-[7px] xl:h-[10px] rounded border-gray-400 transition-transform duration-600 ease-in-out ${!menuAbierto ? "bg-white rotate-0 translate-y-0" : "bg-black rotate-45 translate-y-[300%] md:translate-y-[210%] xl:translate-y-[260%]"}`}>
+            <span className={`absolute top-[6px] w-[50px] lg:w-[60px]  h-[5px] lg:h-[7px] xl:h-[10px] xl:w-[85px] xl:top-[8px] rounded border-gray-400 transition-transform duration-600 ease-in-out ${!menuAbierto ? "bg-white rotate-0 translate-y-0" : "bg-black rotate-45 translate-y-4 xl:translate-y-6"}`}>
 
             </span>
-            <span className={` w-[50px] lg:w-[60px] xl:w-[85px] h-[5px] lg:h-[7px] xl:h-[10px] rounded border-gray-400 transition-opacity duration-600 ease-in-out
+            <span className={`absolute top-[22px]  w-[50px] lg:w-[60px] h-[5px] lg:h-[7px] xl:h-[10px] xl:w-[85px] xl:top-[28px] rounded border-gray-400 transition-opacity duration-600 ease-in-out
             ${!menuAbierto ? "bg-white" : "bg-black opacity-0 transition-opacity duration-400"}`}>
 
             </span>
-            <span className={` w-[50px] lg:w-[60px] xl:w-[85px] h-[5px] lg:h-[7px] xl:h-[10px] rounded border-gray-400 transition-transform duration-600 ease-in-out
-            ${!menuAbierto ? "bg-white" : "bg-black -rotate-45 -translate-y-[330%] md:-translate-y-[280%] xl:-translate-y-[260%] 2xl:-translate-y-[290%]"}`}>
+            <span className={`absolute top-[38px] w-[50px] lg:w-[60px]  h-[5px] lg:h-[7px] xl:h-[10px] xl:w-[85px] xl:top-[48px] rounded border-gray-400 transition-transform duration-600 ease-in-out
+            ${!menuAbierto ? "bg-white" : "bg-black -rotate-45 -translate-y-4"}`}>
 
             </span>
         </div>
