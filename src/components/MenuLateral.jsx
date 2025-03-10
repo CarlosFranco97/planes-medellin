89736188cf99e4a6b40ad2370bf33109b7e30fc2
CarlosFranco97 <127879068+CarlosFranco97@users.noreply.github.com>
2025-03-10 14:28:53 -0500

    Se mejoran tamaños de letra y se genera animacion al abrir y cerrar menu

--- src/components/MenuLateral.jsx
+++ src/components/MenuLateral.jsx
@@ -1,7 +1,9 @@
+import { useEffect } from "react";
+
 const MenuLateral = ({menuAbierto}) => {
-    console.log(menuAbierto)
+    
     return (
-            <div className={`flex flex-col justify-around items-center w-full h-screen bg-white opacity-90 text-4xl font-black text-center absolute top-0 sm:text-3xl lg:w-[40%] xl:text-7xl transition-transform duration-900 ease-in-out   ${(!menuAbierto) ? "hidden" : "block" }`}>
+            <div className={`flex flex-col justify-around items-center w-full h-screen bg-white opacity-90 text-4xl p-4 font-black text-center absolute top-0 transition-all duration-900 ease-in-out lg:w-[40%] lg:text-3xl 2xl:text-7xl ${(!menuAbierto) ? "translate-x-full invisible opacity-0" : "translate-x-0 visible" }`}>
             <a href="">
                 Tour guatapé
             </a>
