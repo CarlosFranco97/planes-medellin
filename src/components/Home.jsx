2ab76d6b5895b716cdbae9c898cc0c5a9816dc22
CarlosFranco97 <127879068+CarlosFranco97@users.noreply.github.com>
2025-03-10 15:04:57 -0500

    Se hace pull del proyecto desde la rama master

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <section className="w-full flex flex-col justify-start items-center h-80 sm:h-auto lg:h-auto lg:justify-evenly">
-            <h1 className="text-yellow-300 text-center w-full font-extrabold text-5xl mt-5 sm:text-8xl lg:text-9xl lg:mt-0 xl:text-[15rem]">
+            <h1 className="text-yellow-300 text-center w-full font-extrabold text-5xl mt-5 sm:text-8xl lg:text-9xl lg:mt-0 xl:text-[15rem] 2xl:text-[18rem]">
                 ¿NO SABES QUE HACER EN MEDELLÍN?
             </h1>
             <button className="text-white bg-[#0080B9]/50 w-80 rounded-2xl text-xl cursor-pointer h-15 hover:bg-[#0080B9]/30 transition-all duration-900 sm:h-20 sm:w-120 sm:text-3xl font-black cursor-pointer mt-10 lg:text-4xl lg:w-150 lg:mt-30 lg:h-30 xl:text-[4rem] xl:w-260 xl:h-50">
