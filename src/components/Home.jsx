2369824c45adc6911522c65239eed7a63860ed4e
CarlosFranco97 <127879068+CarlosFranco97@users.noreply.github.com>
2025-03-13 20:09:10 -0500

    Se ajusta el home a pantallas medianas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
     return (
         <section className="flex justify-center w-full h-[400px] lg:h-[900px] sm:min-h-screen 2xl:min-h-screen">
 
-            <div className="flex flex-col justify-start gap-20 items-center p-4 max-w-full h-full lg:flex lg:flex-col lg:justify-evenly 2xl:justify-center 2xl:gap-50 2xl:min-h-screen">
+            <div className="flex flex-col justify-start gap-20 items-center p-4 max-w-full h-full md:justify-center lg:justify-evenly 2xl:justify-center 2xl:gap-50 2xl:min-h-screen">
 
                 <h1 className="text-yellow-300 text-center max-w-[1600px] font-extrabold text-5xl sm:text-8xl lg:text-9xl xl:text-[10rem] 2xl:text-[11rem]">
                     ¿NO SABES QUE HACER EN MEDELLÍN?
